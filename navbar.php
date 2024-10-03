11012cbd92d51dbe778741e5516abb9390415ba9
MelissaPatton <pattonm1@etown.edu>
2024-10-03 13:42:23 -0400

    added Bootstrap code for Nav Bar

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">YourBrand</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="schedule.html">Schedule</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="report.html">Report</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
