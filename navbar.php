704da79768feb3751ae14be8d948735f18df3c34
MelissaPatton <pattonm1@etown.edu>
2024-10-06 12:49:49 -0400

    Updated nav bar name

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <!-- navbar.php -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">YourBrand</a>
+    <a class="navbar-brand" href="#">Etown Course Planner</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
