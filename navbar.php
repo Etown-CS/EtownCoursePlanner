cfb9438f04e2c410c85dc92496239aa8686f7a46
MelissaPatton <pattonm1@etown.edu>
2024-10-14 13:54:44 -0400

    updated nav bar

--- navbar.php
+++ navbar.php
@@ -6,15 +6,20 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
+          <a class="nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="schedule.php">Schedule</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="report.php">Report</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="report.php"> 
+            <img src="images/profile.png" alt="Profile" style="height:30px;">
+          </a>
         </li>
       </ul>
     </div>
