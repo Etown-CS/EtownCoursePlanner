fb95ac1ab9b4657e7d8007eabe0f8d604388521c
Dani <dannysue2020@gmail.com>
2024-12-05 13:39:05 -0500

    navbar buttons changing depending on logged in or not

--- web_src/includes/main.js
+++ web_src/includes/main.js
@@ -4,14 +4,31 @@
     window.addEventListener("load", init);
     
     function init() {
+        const email = window.sessionStorage.getItem('email');
         $('#navbar').load('includes/navbar.html', function() {
             // Now that the navbar is loaded, check for the current page and apply "active" class
             document.querySelectorAll('.nav-link').forEach(link => {
                 if (link.href === window.location.href) {
                     link.classList.add('active');
                 }
-            });
+            })
+            if (email) {
+                document.getElementById("li-pfp").classList.remove("hidden");
+                document.getElementById("pfp").classList.add("hidden");
+
+                document.getElementById("li-home").classList.remove("hidden");
+                document.getElementById("home").classList.add("hidden");
+            }  else {
+                // If no email, make sure the profile pictures are in the default state
+                document.getElementById("li-pfp").classList.add("hidden");
+                document.getElementById("pfp").classList.remove("hidden");
+
+                document.getElementById("home").classList.remove("hidden");
+                document.getElementById("li-home").classList.add("hidden");
+            }
         });
+
+        
 
         // Load the footer
         $('#footer').load('includes/footer.html');
