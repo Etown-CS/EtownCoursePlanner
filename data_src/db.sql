510c0158d9377998c622c646efc56348b47b0bf2
IsabelM8 <isabelmattivi.05@gmail.com>
2025-01-23 15:23:42 -0500

    Fixing schedule table

--- data_src/db.sql
+++ data_src/db.sql
@@ -55,6 +55,7 @@ CREATE TABLE IF NOT EXISTS schedule ( -- Saved schedules
 	id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL, 
     name VARCHAR(255) NOT NULL,
+    img VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
